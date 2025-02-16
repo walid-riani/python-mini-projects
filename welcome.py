c6b6b5c7a51f6ec2b00d301557037bbd6ba70bab
Walid Riani <walid-riani@live.nl>
2025-02-16 21:18:38 +0100

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -1,7 +1,7 @@
 line01 = "********************"   #  header  /  footer
 line02 = "*                  *"   # re-use
 line03 = "*     WELCOME!     *"   
-        line04 = "******"  
+        line04 = "******"  #  issue oplossen met extensions/formatter via VScode
 
 
 #  starts with a blank line
