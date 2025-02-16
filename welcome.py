9d1b72bc3a20434357f1c4e919267ed8749f1d89
Walid Riani <walid-riani@live.nl>
2025-02-16 21:17:28 +0100

    welcome file

---
+++ welcome.py
@@ -0,0 +1,13 @@
+line01 = "********************"   #  header  /  footer
+line02 = "*                  *"   # re-use
+line03 = "*     WELCOME!     *"   
+        line04 = "******"  
+
+
+#  starts with a blank line
+print('')
+print(line01)
+print(line02)
+print(line03)
+print(line02)
+print(line01)
